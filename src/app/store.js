585d846a49daec9df5782e6babaf6691b15aae27
Valentino <valentinoindorato@gmail.com>
2024-02-10 22:17:32 -0300

    correction of middleware. The previous one was the correction of the methods

--- src/app/store.js
+++ src/app/store.js
@@ -3,11 +3,12 @@ import { setupListeners } from '@reduxjs/toolkit/query'
 import toDoReducer from "../features/Slice"
 import ImageReducer from "../features/ImageSlice";
 import { listTodoApi } from './services/listToDoServices';
+import { ListGoalsApi } from './services/listGoalsServices';
 
 export const store = configureStore({
-  reducer: {toDo:toDoReducer, image:ImageReducer,[listTodoApi.reducerPath]: listTodoApi.reducer, },
+  reducer: {toDo:toDoReducer, image:ImageReducer,[listTodoApi.reducerPath]: listTodoApi.reducer, [ListGoalsApi.reducerPath]: ListGoalsApi.reducer,},
   middleware: (getDefaultMiddleware) =>
-  getDefaultMiddleware().concat(listTodoApi.middleware),
+  getDefaultMiddleware().concat(ListGoalsApi.middleware, listTodoApi.middleware),
 
 })
 
