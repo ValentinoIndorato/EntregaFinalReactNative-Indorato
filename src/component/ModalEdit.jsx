81dc62018298ec06e9cfdf66279a18a93e404c46
Valentino <valentinoindorato@gmail.com>
2024-02-13 12:37:32 -0300

    use of update function to edit the information desired by the task user

--- src/component/ModalEdit.jsx
+++ src/component/ModalEdit.jsx
@@ -6,7 +6,8 @@ import {
   Button,
   FlatList,
   Modal,
-  TouchableOpacity
+  TouchableOpacity,
+  Switch
 } from "react-native";
 import { useDispatch } from 'react-redux'
 
@@ -18,29 +19,60 @@ import { useUpdateOneTodoMutation } from "../app/services/listToDoServices";
 function ModalEdit({ modalVisible, setModalVisible, item, modalType, index }) {
   const dispatch = useDispatch()
   const [upDateOneToDo] = useUpdateOneTodoMutation()
-  const {title, description, category,  id}=item
-  const [editText, setEditText] = useState("");
-
+  const {title, description, category,done, id}=item
+  const [editText, setNewEditText] = useState("");
+  const [newTitle, setNewTitle]= useState(title)
+  const [newDescription,setNewDescription]= useState(description)
+  const [newCategory, setNewCategory]= useState(category)
+  const [newDone,setDone]= useState(done)
+  const toggleSwitch = () => (setDone(previousState => !previousState));
+  const resetState=()=>{
+    setNewTitle(title)
+    setNewDescription(description)
+    setNewCategory(category)
+    setDone(done)
+}
   return (
     <>
       <Modal visible={modalVisible} transparent={true} style={styles.modal}>
         <View style={styles.conteinerItem}>
-        <TouchableOpacity onPress={() => setModalVisible(false)}><Text >adsad </Text></TouchableOpacity>
+        <TouchableOpacity onPress={() => (setModalVisible(false), resetState())}><Text >adsad </Text></TouchableOpacity>
           <Text style={styles.tex}>{title} </Text>
-          <TextInput
-            placeholder="editar"
-            value={editText === "" ? "title" : editText}
+          <TextInput style={styles.input}
+            placeholder="Título"
+            value={newTitle? newTitle : "Título"}
             onChangeText={(e) => {
-              setEditText(e);
-            }}
-            style={styles.input}
-            focusable
-          />
+              setNewTitle(e);
+            }} ></TextInput>
+            <TextInput style={styles.input}
+            value={newDescription? newDescription:"q"}
+            placeholder="Descripción"
+            onChangeText={(e) => {
+              setNewDescription(e);
+            }}></TextInput>
+            <TextInput style={styles.input}
+            value={category!==undefined &&newCategory==category? category[0]:newCategory}
+            placeholder="Categoría"
+            onChangeText={(e) => {
+              setNewCategory(e);
+            }}></TextInput>
+                            <Text>{newDone?'Realizada' : 'Por realizar'}</Text>
+
+           <Switch
+            trackColor={{false: '#767577', true: '#81b0ff'}}
+            thumbColor={newDone ? '#f5dd4b' : '#f4f3f4'}         
+            value={newDone}
+            onValueChange={toggleSwitch}
+            />
 
           <Button style={styles.button}
             title={modalType === true ? "Editar" : "Borrar"}
             color="#747bff"
-            onPress={() => { setModalVisible(false), modalType === true && dispatch(onHandlerEdit({ text: editText === "" ? title : editText, index: index })), upDateOneToDo({ editText }) }}
+            onPress={() => ( setModalVisible(false), 
+              modalType === true &&
+               dispatch(onHandlerEdit({ text: editText === "" ? title : editText, index: index })),
+               upDateOneToDo({...item, id, title:newTitle, description:newDescription, category:[newCategory],done:newDone}) )
+                }
           />
 
         </View>
