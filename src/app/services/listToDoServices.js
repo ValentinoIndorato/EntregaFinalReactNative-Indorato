f56e5cb69ec9a0baa171007120f2593fa1d3981f
Valentino <valentinoindorato@gmail.com>
2024-02-13 01:39:28 -0300

    implementation of invalidTags Redux

--- src/app/services/listToDoServices.js
+++ src/app/services/listToDoServices.js
@@ -1,23 +1,29 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { base_url } from '../../firabase/db'
+import { queryCache } from '@reduxjs/toolkit/query/react';
 
 export const listTodoApi = createApi({
   reducerPath: 'listTodoApi',
   baseQuery: fetchBaseQuery({ baseUrl: base_url }),
+  tagTypes: ['Post'],
+
   endpoints: (builder) => ({
     getListToDo: builder.query({
       query: () => `ListToDo.json`,
     }),
     getOneToDo: builder.query({
-      query: () => `ListToDo.json`,
-       // query:(id)=>`product/${id}.json`,
+      //query: () => `ListToDo.json`,
+       //query:(id)=>`ListToDo/${id}.json`,
+       query:(id)=>`ListToDo.json?orderBy="done"&equalTo=${id}`,
+      providesTags: ['Post'],
+
     }),
    postOneToDo: builder.mutation({
       query(data) {
         const { id, ...body } = data
        
        return{ url: `ListToDo/${id}.json`,// funciona asignandole despues de la barra algo ese va ser como su id
-          method: 'PUT',//con el metodo post me genera la id automatica y hace el proble de un objeto dentro de otro con el metodo put no
+          method: 'POST',//con el metodo post me genera la id automatica y hace el proble de un objeto dentro de otro con el metodo put no
           body:data,
         }
       //Lo que se envia debe ser un objeto al menos, si es un string solo no lo toma fire
@@ -29,13 +35,16 @@ export const listTodoApi = createApi({
       
     }),
     updateOneTodo: builder.mutation({
-      query(data) {
-        const { id, ...body } = data
-        return {
-          url: `ListToDo/${id}.json`,
+      query:(data)=> ({
+    
+          url: `ListToDo/${data.id}.json`,
           method: 'PUT',
           body:data,
-        }},
+        }),
+        /*isSuccess: () => {
+          queryCache.invalidateQueries('getOneToDo');
+        },*/
+        invalidatesTags: ['Post'],
         //invalidatesTags: (result, error, { id }) => [{ type: 'OneTodo', id }],//PREGUNTAR A CRIS
     })
   }),
